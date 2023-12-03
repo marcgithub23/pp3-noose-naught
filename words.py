da9d9f02c48a16ec8e114274b781b5e35a31e891
marcgithub23 <marc.dagohoy2@gmail.com>
2023-12-03 21:07:21 +0000

    Add a dict of words with  definitions

---
+++ words.py
@@ -0,0 +1,7 @@
+animals = {
+    "dog": "A domesticated carnivorous mammal, typically loyal and friendly.",
+    "cat": "A domesticated carnivorous mammal, known for being independent and agile.",
+    "elephant": "A large herbivorous mammal with a trunk and tusks, native to Africa and Asia.",
+    "penguin": "A flightless aquatic bird, known for its black and white plumage.",
+    "tiger": "A large predatory cat with a distinctive orange coat and black stripes."
+}
